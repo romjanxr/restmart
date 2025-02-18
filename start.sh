9d02add3dacc9bbbed8fcd4f88706774abe9169d
Romjan Ali <romjanvr5@gmail.com>
2025-02-18 15:10:25 +0600

    Using uvicorn instead of daphne

--- start.sh
+++ start.sh
@@ -3,11 +3,14 @@
 echo "Installing dependencies with pip..."
 pip3 install -r requirements.txt
 
+echo "Upgrading pip..."
+python3 -m pip install --upgrade pip
+
 echo "Applying database migrations..."
 python3 manage.py migrate --noinput
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
 
-echo "Starting Daphne ASGI server..."
-daphne -b 0.0.0.0 -p $PORT restmart.asgi:application
+echo "Starting Uvicorn ASGI server..."
+uvicorn restmart.asgi:application --host 0.0.0.0 --port $PORT
