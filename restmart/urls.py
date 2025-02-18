0a4f0e6806a5bd7f2639b9da35c3281e40ea2dd3
Romjan Ali <romjanvr5@gmail.com>
2025-02-18 14:40:11 +0600

    Adding debug toolbar conditionaly

--- restmart/urls.py
+++ restmart/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 from django.conf.urls.static import static
 from django.conf import settings
+if settings.DEBUG == True:
+    from debug_toolbar.toolbar import debug_toolbar_urls
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -27,8 +28,9 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
     path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json' )
-] + debug_toolbar_urls()
+] 
 
 if settings.DEBUG == True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
+    urlpatterns += debug_toolbar_urls()
