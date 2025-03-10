558dbe0028fbb8b708c178a48b1fd023159ad115
Romjan Ali <romjanvr5@gmail.com>
2025-03-10 23:08:10 +0600

    Userserializer updated for put and patch request

--- users/serializers.py
+++ users/serializers.py
@@ -2,12 +2,16 @@ from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer,
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
+    """Used for user registration"""
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id',  'email', 'password', 'first_name',
                   'last_name', 'address', 'phone_number']
 
 
 class UserSerializer(BaseUserSerializer):
+    """Used for retrieving and updating user details"""
     class Meta(BaseUserSerializer.Meta):
         fields = ['id',  'email', 'first_name',
                   'last_name', 'address', 'phone_number', 'is_staff']
+        # Prevent users from modifying these fields
+        read_only_fields = ['email', 'is_staff']
